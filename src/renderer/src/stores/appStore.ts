b8d9848d571f999d953fde590e5ff24c3e020f1f
Glenn <noexgp@gmail.com>
2026-05-26 22:03:52 +0800

    feat: add terminalId and terminalConfig to appStore

--- src/renderer/src/stores/appStore.ts
+++ src/renderer/src/stores/appStore.ts
@@ -6,24 +6,42 @@ export interface BusinessSettings {
   name: string
 }
 
+export interface TerminalConfig {
+  id: string
+  name: string
+  lx310PrinterName: string | null
+  formRowOffset: number
+  formColOffset: number
+  paperWidth: number
+  paperHeight: number
+}
+
 interface AppStore {
   serverUrl: string
   setupComplete: boolean
+  terminalId: string
   currentUser: CurrentUser | null
   businessSettings: BusinessSettings | null
+  terminalConfig: TerminalConfig | null
   setServerUrl: (url: string) => void
   setSetupComplete: (v: boolean) => void
+  setTerminalId: (id: string) => void
   setCurrentUser: (u: CurrentUser | null) => void
   setBusinessSettings: (s: BusinessSettings) => void
+  setTerminalConfig: (c: TerminalConfig | null) => void
 }
 
 export const useAppStore = create<AppStore>((set) => ({
   serverUrl: '',
   setupComplete: false,
+  terminalId: '',
   currentUser: null,
   businessSettings: null,
+  terminalConfig: null,
   setServerUrl: (serverUrl) => set({ serverUrl }),
   setSetupComplete: (setupComplete) => set({ setupComplete }),
+  setTerminalId: (terminalId) => set({ terminalId }),
   setCurrentUser: (currentUser) => set({ currentUser }),
   setBusinessSettings: (businessSettings) => set({ businessSettings }),
+  setTerminalConfig: (terminalConfig) => set({ terminalConfig }),
 }))
